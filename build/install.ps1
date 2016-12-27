c9f1bc3d48ec6ebde6fcc92a3c312a3e75311fb8
mattbrailsford <me@mattbrailsford.com>
2016-12-27 20:21:42 +0000

    Added appvayor build config

---
+++ build/install.ps1
@@ -0,0 +1,19 @@
+param($installPath, $toolsPath, $package, $project)
+
+$appPluginsFolder = $project.ProjectItems | Where-Object { $_.Name -eq "App_Plugins" }
+$contentFolder = $appPluginsFolder.ProjectItems | Where-Object { $_.Name -eq "StackedContent" }
+
+if (!$contentFolder)
+{
+	$newPackageFiles = "$installPath\Content\App_Plugins\StackedContent"
+
+	$projFile = Get-Item ($project.FullName)
+	$projDirectory = $projFile.DirectoryName
+	$projectPath = Join-Path $projDirectory -ChildPath "App_Plugins"
+	$projectPathExists = Test-Path $projectPath
+
+	if ($projectPathExists) {	
+		Write-Host "Updating Stacked Content App_Plugin files using PS as they have been excluded from the project"
+		Copy-Item $newPackageFiles $projectPath -Recurse -Force
+	}
+}
