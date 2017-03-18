07ba627ba3df9754dfd88de31cd10e5670ee5354
mattbrailsford <me@mattbrailsford.com>
2017-03-18 13:33:49 +0000

    Ditched the custome PropertyValueEditor in favour of the new SimpleInnerContentPropertyValueEditor

--- src/Our.Umbraco.StackedContent/PropertyEditors/StackedContentPropertyEditor.cs
+++ src/Our.Umbraco.StackedContent/PropertyEditors/StackedContentPropertyEditor.cs
@@ -1,11 +1,6 @@
 ﻿using System.Collections.Generic;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Our.Umbraco.InnerContent.PropertyEditors;
-using Umbraco.Core.Models;
-using Umbraco.Core.Models.Editors;
 using Umbraco.Core.PropertyEditors;
-using Umbraco.Core.Services;
 
 namespace Our.Umbraco.StackedContent.PropertyEditors
 {
@@ -64,71 +59,7 @@ namespace Our.Umbraco.StackedContent.PropertyEditors
 
         protected override PropertyValueEditor CreateValueEditor()
         {
-            return new StackPropertyValueEditor(base.CreateValueEditor());
-        }
-
-        internal class StackPropertyValueEditor : InnerContentPropertyValueEditorWrapper
-        {
-            public StackPropertyValueEditor(PropertyValueEditor wrapped)
-                : base(wrapped)
-            { }
-
-            public override string ConvertDbToString(Property property, PropertyType propertyType, IDataTypeService dataTypeService)
-            {
-                // Convert / validate value
-                if (property.Value == null || string.IsNullOrWhiteSpace(property.Value.ToString()))
-                    return string.Empty;
-
-                var value = JsonConvert.DeserializeObject<JArray>(property.Value.ToString());
-                if (value == null)
-                    return string.Empty;
-
-                // Process value
-                ConvertInnerContentDbToString(value);
-
-                // Update the value on the property
-                property.Value = JsonConvert.SerializeObject(value);
-
-                // Pass the call down
-                return base.ConvertDbToString(property, propertyType, dataTypeService);
-            }
-
-            public override object ConvertDbToEditor(Property property, PropertyType propertyType, IDataTypeService dataTypeService)
-            {
-                // Convert / validate value
-                if (property.Value == null || string.IsNullOrWhiteSpace(property.Value.ToString()))
-                    return string.Empty;
-
-                var value = JsonConvert.DeserializeObject<JArray>(property.Value.ToString());
-                if (value == null)
-                    return string.Empty;
-
-                // Process value
-                ConvertInnerContentDbToEditor(value);
-
-                // Update the value on the property
-                property.Value = JsonConvert.SerializeObject(value);
-
-                // Pass the call down
-                return base.ConvertDbToEditor(property, propertyType, dataTypeService);
-            }
-
-            public override object ConvertEditorToDb(ContentPropertyData editorValue, object currentValue)
-            {
-                // Convert / validate value
-                if (editorValue.Value == null || string.IsNullOrWhiteSpace(editorValue.Value.ToString()))
-                    return null;
-
-                var value = JsonConvert.DeserializeObject<JArray>(editorValue.Value.ToString());
-                if (value == null || value.Count == 0)
-                    return null;
-
-                // Process value
-                ConvertInnerContentEditorToDb(value);
-
-                // Return value
-                return JsonConvert.SerializeObject(value);
-            }
+            return new SimpleInnerContentPropertyValueEditor(base.CreateValueEditor());
         }
 
         #endregion
