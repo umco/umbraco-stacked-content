2d780bf8ce86a8a614e92a7f031bc3b387f57715
leekelleher <leekelleher@gmail.com>
2018-02-22 21:01:29 +0000

    Small refactor to StackedContentValueConverter
    Adds early exits and saves making the call to `source.ToString()` twice.

--- src/Our.Umbraco.StackedContent/Converters/StackedContentValueConverter.cs
+++ src/Our.Umbraco.StackedContent/Converters/StackedContentValueConverter.cs
@@ -22,14 +22,18 @@ namespace Our.Umbraco.StackedContent.Converters
 
         public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
         {
+            if (source == null)
+                return null;
+
+            var str = source.ToString();
+            if (string.IsNullOrWhiteSpace(str))
+                return null;
+
             try
             {
-                if (source != null && !source.ToString().IsNullOrWhiteSpace())
-                {
-                    var rawValue = JsonConvert.DeserializeObject<JArray>(source.ToString());
+                var rawValue = JsonConvert.DeserializeObject<JArray>(str);
 
-                    return ConvertInnerContentDataToSource(rawValue, null, 1, preview);
-                }
+                return ConvertInnerContentDataToSource(rawValue, null, 1, preview);
             }
             catch (Exception ex)
             {
