05bd9691214fa7e1071ad1c92455d2dd452bd117
leekelleher <leekelleher@gmail.com>
2018-02-22 11:51:14 +0000

    ViewHelper - StringWriter disposal
    Adds a `using` statement to the StringWriter

--- src/Our.Umbraco.StackedContent/Web/Helpers/ViewHelper.cs
+++ src/Our.Umbraco.StackedContent/Web/Helpers/ViewHelper.cs
@@ -11,29 +11,31 @@ namespace Our.Umbraco.StackedContent.Web.Helpers
 
         public static string RenderPartial(string partialName, object model, string[] viewLocations)
         {
-            var sw = new StringWriter();
-            var httpContext = new HttpContextWrapper(HttpContext.Current);
-
-            var routeData = new RouteData();
-            routeData.Values.Add("controller", "DummyController");
-
-            var controllerContext = new ControllerContext(new RequestContext(httpContext, routeData), new DummyController());
-
-            var viewEngine = new RazorViewEngine
+            using (var sw = new StringWriter())
             {
-                PartialViewLocationFormats = viewLocations
-            };
+                var httpContext = new HttpContextWrapper(HttpContext.Current);
 
-            var viewResult = viewEngine.FindPartialView(controllerContext, partialName, false);
-            if (viewResult.View == null)
-            {
-                //TODO: Log lack of view?
-                return null;
+                var routeData = new RouteData();
+                routeData.Values.Add("controller", "DummyController");
+
+                var controllerContext = new ControllerContext(new RequestContext(httpContext, routeData), new DummyController());
+
+                var viewEngine = new RazorViewEngine
+                {
+                    PartialViewLocationFormats = viewLocations
+                };
+
+                var viewResult = viewEngine.FindPartialView(controllerContext, partialName, false);
+                if (viewResult.View == null)
+                {
+                    // TODO: Log lack of view?
+                    return null;
+                }
+
+                viewResult.View.Render(new ViewContext(controllerContext, viewResult.View, new ViewDataDictionary { Model = model }, new TempDataDictionary(), sw), sw);
+
+                return sw.ToString();
             }
-
-            viewResult.View.Render(new ViewContext(controllerContext, viewResult.View, new ViewDataDictionary { Model = model }, new TempDataDictionary(), sw), sw);
-
-            return sw.ToString();
         }
     }
 }
