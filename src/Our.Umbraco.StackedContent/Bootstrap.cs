b8899a57dadb387d6339163b960f430ecdccd412
leekelleher <leekelleher@gmail.com>
2018-02-22 21:14:20 +0000

    Adds migration for v1.1.0
    When upgrading to (upcoming) StackedContent (and InnerContent) v1.1.0, the user would need to re-save all the StackedContent data-types,
    in order to swap over the doc-type aliases with GUIDs.
    
    This migration would automate that step.
    
    This pull request depends on a pull request in the InnerContent repository:
    https://github.com/umco/umbraco-inner-content/pull/12

---
+++ src/Our.Umbraco.StackedContent/Bootstrap.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Our.Umbraco.InnerContent.Helpers;
+using Our.Umbraco.StackedContent.PropertyEditors;
+using Semver;
+using Umbraco.Core;
+
+namespace Our.Umbraco.StackedContent
+{
+    public class Bootstrap : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            ApplyMigrations(applicationContext);
+        }
+
+        private void ApplyMigrations(ApplicationContext applicationContext)
+        {
+            var productName = StackedContentPropertyEditor.PropertyEditorAlias;
+            var currentVersion = new SemVersion(0, 0, 0);
+            var targetVersion = GetApplicationVersion();
+
+            MigrationHelper.ApplyMigrations(applicationContext, productName, currentVersion, targetVersion);
+        }
+
+        private SemVersion GetApplicationVersion()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            if (assembly != null)
+            {
+                var info = FileVersionInfo.GetVersionInfo(assembly.Location);
+                if (info != null
+                    && string.IsNullOrWhiteSpace(info.ProductVersion) == false
+                    && SemVersion.TryParse(info.ProductVersion, out SemVersion applicationVersion))
+                {
+                    return applicationVersion;
+                }
+            }
+
+            return new SemVersion(0, 0, 0);
+        }
+    }
+}
---
+++ src/Our.Umbraco.StackedContent/Migrations/TargetOneOneZero/UpdateDataTypes.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using Our.Umbraco.StackedContent.PropertyEditors;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Persistence.Migrations;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Our.Umbraco.StackedContent.Migrations.TargetOneOneZero
+{
+    [Migration("1.1.0", 1, StackedContentPropertyEditor.PropertyEditorAlias)]
+    public class UpdateDataTypes : MigrationBase
+    {
+        public UpdateDataTypes(ISqlSyntaxProvider sqlSyntax, ILogger logger)
+            : base(sqlSyntax, logger)
+        { }
+
+        public override void Down()
+        { }
+
+        public override void Up()
+        {
+            var contentTypeService = ApplicationContext.Current.Services.ContentTypeService;
+            var dataTypeService = ApplicationContext.Current.Services.DataTypeService;
+
+            // Find all the StackedContent data-types
+            var dataTypes = dataTypeService.GetDataTypeDefinitionByPropertyEditorAlias(StackedContentPropertyEditor.PropertyEditorAlias);
+            if (dataTypes == null)
+                return;
+
+            foreach (var dataType in dataTypes)
+            {
+                var requiresSave = false;
+
+                var preValues = dataTypeService.GetPreValuesCollectionByDataTypeId(dataType.Id);
+                if (preValues == null)
+                    continue;
+
+                var config = preValues.PreValuesAsDictionary;
+                if (config.ContainsKey("contentTypes"))
+                {
+                    // Get all the saved doc-type aliases
+                    var contentTypes = config["contentTypes"];
+                    var json = contentTypes.Value;
+                    if (string.IsNullOrWhiteSpace(json))
+                        continue;
+
+                    // Replace the doc-type alias with the doc-type GUID
+                    var options = JsonConvert.DeserializeObject<ContentTypeOption[]>(json);
+                    if (options == null)
+                        continue;
+
+                    foreach (var option in options)
+                    {
+                        // If the prevalue already has a GUID, then skip it
+                        if (string.IsNullOrWhiteSpace(option.icContentTypeGuid) == false)
+                            continue;
+
+                        if (string.IsNullOrWhiteSpace(option.icContentTypeAlias))
+                            continue;
+
+                        var docType = contentTypeService.GetContentType(option.icContentTypeAlias);
+                        if (docType == null)
+                            continue;
+
+                        option.icContentTypeGuid = docType.Key.ToString();
+                        requiresSave = true;
+                    }
+
+                    // Save the prevalue and data-type
+                    if (requiresSave)
+                    {
+                        contentTypes.Value = JsonConvert.SerializeObject(options);
+                        dataTypeService.SaveDataTypeAndPreValues(dataType, config);
+                    }
+                }
+            }
+        }
+
+#pragma warning disable IDE1006 // Naming Styles
+        public class ContentTypeOption
+        {
+            public string icContentTypeAlias { get; set; }
+            public string icContentTypeGuid { get; set; }
+            public string nameTemplate { get; set; }
+        }
+#pragma warning restore IDE1006 // Naming Styles
+    }
+}
