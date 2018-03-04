0668375ecb629ec6777e172be0829af89fea782c
leekelleher <leekelleher@gmail.com>
2018-03-04 22:52:35 +0000

    Amends based on Matt's code review
    I've had to also include the `ContentTypes` field, as if we don't, the inherited property will be displayed at the bottom of the prevalue fields.

--- src/Our.Umbraco.StackedContent/PropertyEditors/StackedContentPropertyEditor.cs
+++ src/Our.Umbraco.StackedContent/PropertyEditors/StackedContentPropertyEditor.cs
@@ -10,6 +10,33 @@ namespace Our.Umbraco.StackedContent.PropertyEditors
 
         public StackedContentPropertyEditor()
             : base()
-        { }
+        {
+            DefaultPreValues.Add("maxItems", 0);
+            DefaultPreValues.Add("singleItemMode", "0");
+            DefaultPreValues.Add("disablePreview", "0");
+        }
+
+        protected override PreValueEditor CreatePreValueEditor()
+        {
+            return new StackedContentPreValueEditor();
+        }
+
+        internal class StackedContentPreValueEditor : SimpleInnerContentPreValueEditor
+        {
+            [PreValueField("contentTypes", "Content Types", "~/App_Plugins/InnerContent/views/innercontent.doctypepicker.html", Description = "Select the content types to use as the data blueprint.")]
+            public new string[] ContentTypes { get; set; }
+
+            [PreValueField("maxItems", "Max Items", "number", Description = "Set the maximum number of items allowed in this stack.")]
+            public string MaxItems { get; set; }
+
+            [PreValueField("singleItemMode", "Single Item Mode", "boolean", Description = "Set whether to work in single item mode (only the first defined Content Type will be used).")]
+            public string SingleItemMode { get; set; }
+
+            [PreValueField("hideLabel", "Hide Label", "boolean", Description = "Set whether to hide the editor label and have the list take up the full width of the editor window.")]
+            public string HideLabel { get; set; }
+
+            [PreValueField("disablePreview", "Disable Preview", "boolean", Description = "Set whether to disable the preview of the items in the stack.")]
+            public string DisablePreview { get; set; }
+        }
     }
 }
