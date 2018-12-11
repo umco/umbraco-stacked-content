b1df94a8d9979a09e5628a80f42abef7025ff3ac
Andy Felton <andy.felton@nemisys.uk.com>
2018-12-11 08:35:54 +0000

    Create published Content Request prior to Rendering to support macros in RTS

--- src/Our.Umbraco.StackedContent/Web/Controllers/StackedContentApiController.cs
+++ src/Our.Umbraco.StackedContent/Web/Controllers/StackedContentApiController.cs
@@ -1,13 +1,18 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Mime;
+using System.Web;
 using System.Web.Http;
+using System.Web.Security;
 using Newtonsoft.Json.Linq;
 using Our.Umbraco.InnerContent.Helpers;
 using Our.Umbraco.StackedContent.Models;
 using Our.Umbraco.StackedContent.Web.Helpers;
+using Umbraco.Core.Configuration;
 using Umbraco.Core.Models;
 using Umbraco.Web.Mvc;
+using Umbraco.Web.Routing;
 using Umbraco.Web.WebApi;
 
 namespace Our.Umbraco.StackedContent.Web.Controllers
@@ -30,6 +35,22 @@ namespace Our.Umbraco.StackedContent.Web.Controllers
                     // If unpublished, then fake PublishedContent (with IContent object)
                     page = new UnpublishedContent(pageId, Services);
                 }
+
+                var baseUrl = HttpContext.Current.Request.Url.AbsoluteUri.Replace(HttpContext.Current.Request.Path, "/");
+                var umbUrl = baseUrl + page.Url.TrimStart('/');
+
+                var pcr = new PublishedContentRequest(
+                    new Uri(umbUrl),
+                    UmbracoContext.RoutingContext,
+                    UmbracoConfig.For.UmbracoSettings().WebRouting,
+                    s => Roles.Provider.GetRolesForUser(s)
+                );
+
+                UmbracoContext.PublishedContentRequest = pcr;
+                UmbracoContext.PublishedContentRequest.PublishedContent = page;
+
+                pcr.Prepare();
+
             }
 
             // Convert item
